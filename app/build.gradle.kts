8d86897a3ccc23f4848c0a97f750315e4c4784b4
Sheikh <barsamaniya11@gmail.com>
2024-02-20 21:07:20 +0500

    Updated CreateNewHabitFragment UI by adding date picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,10 @@ dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-paging:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
+
+    // Navigation compenent
+    val nav_version = "2.7.7"
+
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
--- app/src/main/java/com/baxramov/atomichabits/presentation/screens/HabitsListFragment.kt
+++ app/src/main/java/com/baxramov/atomichabits/presentation/screens/HabitsListFragment.kt
@@ -5,20 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.baxramov.atomichabits.R
+import com.baxramov.atomichabits.databinding.FragmentHabitsListBinding
 
 class HabitsListFragment : Fragment() {
+
+    private var _binding: FragmentHabitsListBinding? = null
+    private val binding: FragmentHabitsListBinding
+        get() = _binding ?: throw NullPointerException("FragmentHabitsListBinding is null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_habits_list, container, false)
+    ): View {
+        _binding = FragmentHabitsListBinding.inflate(this.layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_habitsListFragment_to_createNewHabitFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        if (_binding != null) {
+            _binding = null
+        }
+        super.onDestroy()
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() =
-            HabitsListFragment()
+        fun newInstance() = HabitsListFragment()
     }
 }
